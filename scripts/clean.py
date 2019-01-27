bae9dbf53e4a5a5ce2ba1bd183c98220ff8bed68
TomDonoghue <tdonoghue@ucsd.edu>
2019-01-27 12:20:29 -0800

    Clean up - restrict to relevant changes

--- scripts/clean.py
+++ scripts/clean.py
@@ -7,7 +7,6 @@ path_root = op.join(op.dirname(op.abspath(__file__)), '..')
 
 paths = [op.join(path_root, '_site'),
          op.join(path_root, '_build')]
-
 for path in paths:
     print('Removing {}...'.format(path))
     sh.rmtree(path, ignore_errors=True)
