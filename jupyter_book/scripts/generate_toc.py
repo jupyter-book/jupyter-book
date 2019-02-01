a476afab15b6323f6da2d162a68bf0710f0e0200
Chris Holdgraf <choldgraf@berkeley.edu>
2019-01-31 19:20:34 -0800

    simplifying toc

--- jupyter_book/scripts/generate_toc.py
+++ jupyter_book/scripts/generate_toc.py
@@ -28,24 +28,6 @@ def _filename_to_title(filename, split_char='_'):
     title = ' '.join(ii.capitalize() for ii in filename_parts)
     return title
 
-def path_to_dict(path, split_char='_', base_dir=None):
-    # Ensures we propagate the first path to subsequent calls
-    base_dir = path if base_dir is None else base_dir
-
-    # Infer title from the name of the file/folder
-    ext = os.path.splitext(path)[-1]
-    if ('ipynb_checkpoints' in path) or (len(ext) > 0 and ext not in ['.html', '.md', '.ipynb']):
-        # Skip non-content files
-        return "UNSUPPORTED FILE"
-    d = {'title': _filename_to_title(os.path.basename(path), split_char=split_char)}
-    if os.path.isdir(path):
-        new_sections = [path_to_dict(os.path.join(path, ii), base_dir=base_dir, split_char=split_char) for ii in os.listdir(path)]
-        new_sections = [ii for ii in new_sections if ii != "UNSUPPORTED FILE"]
-        if len(new_sections) != 0:
-            d['sections'] = new_sections
-    else:
-        d['url'] = os.path.splitext(path)[0].split(base_dir)[-1]
-    return d
 
 if __name__ == '__main__':
     args = parser.parse_args()
@@ -53,43 +35,58 @@ if __name__ == '__main__':
         args.out_path = None
 
     # Generate YAML from the directory structure
-    toc = path_to_dict(args.content_folder, split_char=args.filename_split_char)
-
-    # Remove empty sections
-    toc = toc['sections']
+    toc = []
+    for ifolder, _, ifiles in os.walk(args.content_folder):
+        path_rel_to_content = ifolder.replace(args.content_folder, '')
+        if any(any(ifile.endswith(ii) for ii in [".ipynb", ".md"]) for ifile in ifiles):
+            toc.append("==================================================")
+        for ifile in ifiles:
+            if not any(ifile.endswith(ii) for ii in [".ipynb", ".md"]):
+                continue
+            i_title = _filename_to_title(ifile, args.filename_split_char)
+            i_url = os.path.join(path_rel_to_content, i_title)
+            toc.append({'title': i_title, 'url': i_url})
 
     # Write the TOC to YAML or print it
     yaml = YAML()
+    top = ("# This file contains the order and numbering for all sections in the book."
+            "# _includes/sidebar.html renders this as a sidebar.\n"
+            "#\n"
+            "# Each entry has the following schema:\n"
+            "#\n"
+            "# title: Title of chapter or section\n"
+            "# url: URL of section. Use absolute URLs to link between pages\n"
+            "# sections: Contains chapter's sections\n"
+            "# not_numbered: true if the section shouldn't have a number in the sidebar\n"
+            "#   (e.g. Introduction or appendices)\n"
+            "# expand_sections: true if you'd like the sections of this chapter to always\n"
+            "#   be expanded in the sidebar.\n"
+            "# divider: true if this entry should just be rendered as a horizontal divider\n"
+            "#   in the sidebar. All other values are ignored.\n")
+    warn = ("#\n"
+            "# ==============================\n"
+            "# AUTOMATICALLY GENERATED TOC FILE.\n"
+            "# You should review the contents of this file, re-order items as you wish,\n"
+            "# and nest chapters in sections if you wish. The ======= symbols represent \n"
+            "# folder breaks.\n"
+            "# \n"
+            "# See the demo `toc.yml` for the right structure to follow. You can \n"
+            "# generate a demo book by running `jupyter-book create mybook --demo`\n"
+            "# ==============================\n")
     if args.out_path is None:
+        print(top + warn)
         yaml.dump(toc, sys.stdout)
     else:
         if os.path.exists(args.out_path) and bool(args.overwrite) is False:
             raise ValueError('toc.yaml file exists, delete the file or set `overwrite=True`')
+
+        # Dump the YAML
         with open(args.out_path, 'w') as ff:
             yaml.dump(toc, ff)
+
+        # Read back in so we can add a comment
         with open(args.out_path, 'r') as ff:
             contents = ff.read()
         with open(args.out_path, 'w') as ff:
-            top = ("# This file contains the order and numbering for all sections in the book."
-                   "# _includes/sidebar.html renders this as a sidebar.\n"
-                   "#\n"
-                   "# Each entry has the following schema:\n"
-                   "#\n"
-                   "# title: Title of chapter or section\n"
-                   "# url: URL of section. Use absolute URLs to link between pages\n"
-                   "# class: (optional) a CSS class to add to this link\n"
-                   "# sections: Contains chapter's sections\n"
-                   "# not_numbered: true if the section shouldn't have a number in the sidebar\n"
-                   "#   (e.g. Introduction or appendices)\n"
-                   "# expand_sections: true if you'd like the sections of this chapter to always\n"
-                   "#   be expanded in the sidebar.\n"
-                   "# divider: true if this entry should just be rendered as a horizontal divider\n"
-                   "#   in the sidebar. All other values are ignored.\n\n")
-            warn = ("# ======\n"
-                    "# AUTOMATICALLY GENERATED TOC FILE.\n"
-                    "# You should review the contents of this file, re-order items as you wish,\n"
-                    "# and remove the empty sections below. In particular, ensure that each\n"
-                    "# item has at least a 'title' and 'url' field defined (which will not be the case for folders)\n"
-                    "# ======\n")
-            ff.write(top+warn + contents)
+            ff.write(top + warn + contents)
         print('TOC written to: {}'.format(args.out_path))
