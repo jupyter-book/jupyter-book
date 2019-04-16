77cb587dbd639cda1acef0b332ae368fb095b743
Chris Holdgraf <choldgraf@berkeley.edu>
2019-04-16 14:49:13 -0700

    updating single doc experiment

--- jupyter_book/commands/report.py
+++ jupyter_book/commands/report.py
@@ -9,6 +9,7 @@ def report():
     parser = argparse.ArgumentParser(description="Generate a report from a jupyter notebook")
     parser.add_argument('path_notebook', help="The path to a Jupyter Notebook")
     parser.add_argument('--path-output', help="The path to the output report")
+    parser.add_argument('--config', default=None, help="The path to a configuration file for this report")
     parser.add_argument('--css',default=None, help="A path to custom CSS to include with the report")
     parser.add_argument('--js', default=None, help="A path to custom JavaScript to include with the report")
     parser.add_argument("--overwrite", default=False, action="store_true", help="Whether to overwrite a pre-existing report if it exists")
@@ -19,4 +20,4 @@ def report():
     path_output_report = op.basename(path_notebook).replace('.ipynb', '') if args.path_output is None else args.path_output
 
 
-    new_report(path_notebook, path_output_report, args.css, args.js, args.overwrite)
+    new_report(path_notebook, path_output_report, args.config, args.css, args.js, args.overwrite)
--- jupyter_book/report.py
+++ jupyter_book/report.py
@@ -10,11 +10,10 @@ from .create import new_book
 from .build import build_book
 from distutils.dir_util import copy_tree
 from glob import glob
+import sass
 
-def new_report(path_notebook, path_output, css, js, overwrite=False):
+def new_report(path_notebook, path_output, config, css, js, overwrite=False):
     """Create a new report from a single notebook."""
-
-    name_notebook = op.basename(path_notebook)
 
     # Check if we've specified a single notebook (and in this case will use the report script)
     if not op.exists(path_notebook):
@@ -22,8 +21,9 @@ def new_report(path_notebook, path_output, css, js, overwrite=False):
     if not path_notebook.endswith('.ipynb'):
         raise ValueError(f"Specified path_notebook file does not end in .ipynb, found {op.splitext(path_notebook)[-1]}")
 
+    name_notebook = op.basename(path_notebook)
 
-    # Remove an old report if we want
+    # Remove an old report
     if op.isdir(path_output):
         if overwrite is True:
             sh.rmtree(path_output)
@@ -35,54 +35,76 @@ def new_report(path_notebook, path_output, css, js, overwrite=False):
     path_temp = op.join(path_output, 'TMP')
     raw_folder = op.join(path_temp, 'raw')
 
-    # Content we'll use to build the report
-    path_config = op.join(raw_folder, '_config.yml')
-    path_content = op.join(raw_folder, 'content')
-    path_out = op.join(path_content, op.basename(path_notebook))
+    # Define paths for content we'll use to build the report
+    path_tmp_config = op.join(raw_folder, '_config.yml')  # Config for the report
+    path_tmp_content = op.join(raw_folder, 'content')  # Content folder for the report book
+    path_tmp_notebook = op.join(path_tmp_content, op.basename(path_notebook))  # Where the report will be generated
+    path_tmp_book = op.join(path_temp, 'book')  # Where the jupyter book for the report is generated
+    path_html = op.join(path_temp, 'html')  # Where the HTML will be built
 
-    # The temporary book goes here
-    path_book = op.join(path_temp, 'book')
+    # Make content directory and copy our notebook
+    os.makedirs(path_tmp_content, exist_ok=True)
+    sh.copy2(path_notebook, path_tmp_notebook)
 
-    # HTML for the built report
-    path_html = op.join(path_temp, 'html')
+    ##### Modify the notebook content ###############################################
 
-    os.makedirs(path_content, exist_ok=True)
-    sh.copy2(path_notebook, path_out)
-
-    ## Hide code cells
-    ntbk = nbf.read(path_out, nbf.NO_CONVERT)
+    ## Hide code cells for the report
+    ntbk = nbf.read(path_tmp_notebook, nbf.NO_CONVERT)
     for cell in ntbk['cells']:
         if cell['cell_type'] == 'code':
             tags = cell['metadata'].get('tags', [])
             if not 'hidecode' in tags:
                 tags.append('hidecode')
             cell['metadata']['tags'] = tags
-    nbf.write(ntbk, path_out)
+    nbf.write(ntbk, path_tmp_notebook)
 
 
     ##### Create book for the report ###############################################
 
     # Add some config for a report
+
+
+    # ##############################################################################
+    # TODO: figure out the updating stuff here
+
+
+
+
     yaml = YAML()
-    config = {'show_sidebar': False, "baseurl": "", "url": ""}
-    with open(path_config, 'w') as ff:
+    default_config = {
+        "show_sidebar": False,
+        "baseurl": "",
+        "url": "",
+        "page_nav": False,
+        "use_binder_button": False,
+        "use_thebelab_button": False,
+        "use_show_widgets_button": False,
+        "use_download_button": False
+    }
+    if config is not None:
+        if not op.exists(config):
+            raise ValueError("Configuration file not found: {}".format(config))
+        with open(config, 'r') as ff:
+            custom_config = yaml.load(ff)
+        default_config.update(custom_config)
+    with open(path_tmp_config, 'w') as ff:
         yaml.dump(config, ff)
 
-    # Create the new book
+    # Create a new book for this report
     print("Creating new book...")
-    new_book(path_book, path_content, None, None, config=path_config, overwrite=overwrite)
+    new_book(path_tmp_book, path_tmp_content, None, None, config=path_tmp_config, overwrite=overwrite)
 
-    # Build the markdown for it
+    # Build the markdown for the book
     print("Building book markdown...")
-    path_toc = op.join(path_book, '_data', 'toc.yml')
-    path_template = op.join(path_book, 'scripts', 'templates', 'jekyllmd.tpl')
-    build_book(path_book, path_toc_yaml=path_toc, path_template=path_template, config_file=path_config)
+    path_toc = op.join(path_tmp_book, '_data', 'toc.yml')
+    path_template = op.join(path_tmp_book, 'scripts', 'templates', 'jekyllmd.tpl')
+    build_book(path_tmp_book, path_toc_yaml=path_toc, path_template=path_template, config_file=path_tmp_config)
 
     # Build the HTML in the temp folder
     print("Building report HTML...")
-    path_html_rel_build_folder = op.relpath(path_html, path_book)
+    path_html_rel_build_folder = op.relpath(path_html, path_tmp_book)
     cmd = f'bundle exec jekyll build -d {path_html_rel_build_folder}'
-    err = run(cmd.split(), check=True, cwd=path_book)
+    run(cmd.split(), check=True, cwd=path_tmp_book)
 
     ##### Clean up HTML ###########################################################
 
