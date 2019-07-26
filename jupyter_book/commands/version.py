d96d21502c1dfb53833c60899aca0ef2fb52a5ff
Chris Holdgraf <choldgraf@berkeley.edu>
2019-07-26 13:54:16 -0700

    newline for version

--- jupyter_book/commands/version.py
+++ jupyter_book/commands/version.py
@@ -1,9 +1,11 @@
-import sys
 import argparse
 from jupyter_book import __version__
+import sys
+
 
 def version():
     parser = argparse.ArgumentParser(
         description="Print the version of Jupyter Book currently in-use.")
-
+    # Args won't be used, just keeping so we don't break PEP
+    parser.parse_args(sys.argv)
     print("Running Jupyter Book version: {}".format(__version__))
