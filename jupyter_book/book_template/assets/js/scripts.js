a344096f217020e60971c7d128961b2bd58ee17f
Chris Holdgraf <choldgraf@berkeley.edu>
2019-08-26 16:24:45 -0700

    tweaks to spacing and layout

--- jupyter_book/book_template/assets/js/scripts.js
+++ jupyter_book/book_template/assets/js/scripts.js
@@ -5,7 +5,7 @@
  * [2] Sidebar toggling
  * [3] Sidebar scroll preserving
  * [4] Keyboard navigation
- * [5] Right sidebar scroll highlighting
+ * [5] Right sidebar scroll highlighting / navbar show
  */
 
 const togglerId = 'js-sidebar-toggle'
@@ -51,7 +51,7 @@ const toggleSidebar = () => {
  * _sass/inuitcss/tools/_tools.mq.scss
  *
  */
-const autoCloseSidebarBreakpoint = 740
+const autoCloseSidebarBreakpoint = 769
 
 // Set up event listener for sidebar toggle button
 const sidebarButtonHandler = () => {
@@ -127,40 +127,29 @@ const initListener = () => {
 initFunction(initListener);
 
 /**
- * [5] Right sidebar scroll highlighting
+ * [5] Scrolling functions:
+ *   * Right sidebar scroll highlighting
+ *   * Top navbar hiding for scrolling
  */
 
-highlightRightSidebar = function() {
-  var position = document.querySelector('.c-textbook__page').scrollTop;
-  position = position + (window.innerHeight / 4);  // + Manual offset
+highlightRightSidebar = function(content) {
 
-  // Highlight the "active" menu item
-  document.querySelectorAll('.c-textbook__content h2, .c-textbook__content h3').forEach((header, index) => {
-      var target = header.offsetTop;
-      var id = header.id;
-      if (position >= target) {
-        var query = 'ul.toc__menu a[href="#' + id + '"]';
-        document.querySelectorAll('ul.toc__menu li').forEach((item) => {item.classList.remove('active')});
-        document.querySelectorAll(query).forEach((item) => {item.parentElement.classList.add('active')});
-    }
-  });
-  document.querySelector('.c-textbook__page').addEventListener('scroll', highlightRightSidebar);
 };
 
-initFunction(highlightRightSidebar);
+decideToShowNavbar = function() {
+  
 
+}
 
-
-/**
- * [6] Top navbar hiding for scrolling
- */
-
-initScrollingNavbar = () => {
+initScrollFunc = function() {
   var content = document.querySelector('.c-textbook__page');
   var topbar = document.getElementById("top-navbar");
+  var prevScrollpos = content.scrollTop; // Initializing
 
-  var prevScrollpos = content.scrollTop;
-  content.onscroll = function() {
+  scrollFunc = function() {
+    var position = content.scrollTop; // Because we use this differently for sidebar
+    
+    // Decide to show the navbar
     var currentScrollPos = content.scrollTop;
     if (prevScrollpos > currentScrollPos) {
       topbar.style.top = "0";
@@ -168,7 +157,27 @@ initScrollingNavbar = () => {
       topbar.style.top = "-200px";
     }
     prevScrollpos = currentScrollPos;
+
+    // Highlight the right sidebar
+    position = position + (window.innerHeight / 4);  // + Manual offset
+  
+    content.querySelectorAll('h2, h3').forEach((header, index) => {
+        if (header.offsetParent.classList.contains('jb_cell')) {
+          // If we're a page made from a notebook, there will be a parent called jb_cell
+          var target = header.offsetParent.offsetTop;
+        } else {
+          var target = header.offsetTop;
+        }
+        
+        var id = header.id;
+        if (position >= target) {
+          var query = 'ul.toc__menu a[href="#' + id + '"]';
+          document.querySelectorAll('ul.toc__menu li').forEach((item) => {item.classList.remove('active')});
+          document.querySelectorAll(query).forEach((item) => {item.parentElement.classList.add('active')});
+      }
+    });
   }
+  content.addEventListener('scroll', scrollFunc);
 }
 
-initFunction(initScrollingNavbar);
+initFunction(initScrollFunc);
