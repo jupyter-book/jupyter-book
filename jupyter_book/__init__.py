7fd7ec4ef5ae58095d32898e7071096a959fa185
Chris Holdgraf <choldgraf@berkeley.edu>
2019-09-24 14:32:25 -0700

    version bump and install fix

--- jupyter_book/__init__.py
+++ jupyter_book/__init__.py
@@ -1,11 +1,13 @@
 """Build an online book using Jupyter Notebooks and Jekyll."""
-import yaml
 import os.path as op
 import os
 
 # Load the version from the template Jupyter Book repository config
 path_yml = op.join(op.dirname(__file__), 'book_template', '_config.yml')
 with open(path_yml, 'r') as ff:
-    __version__ = yaml.safe_load(ff.read()).get(
-        "jupyter_book_version"
-    )
+    # Read in the version *without* pyyaml because we can't assume it's installed
+    lines = ff.readlines()
+
+version = [line for line in lines if 'jupyter_book_version' in line]
+version_line = [line for line in lines if 'jupyter_book_version' in line][0]
+__version__ = version_line.split(' ')[-1].strip().strip('"')
