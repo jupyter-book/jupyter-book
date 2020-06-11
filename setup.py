98c4d35d23459d4c1afb109b456c9549fead7926
Chris Holdgraf <choldgraf@berkeley.edu>
2020-06-11 13:53:45 -0700

    myst version

--- setup.py
+++ setup.py
@@ -49,7 +49,7 @@ setup(
         "pyyaml",
         "docutils>=0.15",
         "sphinx<3",
-        "myst-nb>=0.8.1",
+        "myst-nb~=0.8",
         "click",
         "setuptools",
         "nbformat",
@@ -58,7 +58,7 @@ setup(
         "sphinx_togglebutton",
         "sphinx-copybutton",
         "sphinxcontrib-bibtex",
-        "sphinx_book_theme>=0.0.23",
+        "sphinx_book_theme~=0.0",
     ],
     extras_require={
         "code_style": ["flake8<3.8.0,>=3.7.0", "black", "pre-commit==1.17.0"],
