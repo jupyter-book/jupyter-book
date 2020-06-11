7e7a54e208920534d7db0a1b65bc34c1f04e54ad
Chris Holdgraf <choldgraf@berkeley.edu>
2020-06-11 11:55:23 -0700

    fixing topbar repo buttons

--- setup.py
+++ setup.py
@@ -58,7 +58,7 @@ setup(
         "sphinx_togglebutton",
         "sphinx-copybutton",
         "sphinxcontrib-bibtex",
-        "sphinx_book_theme>=0.0.21",
+        "sphinx_book_theme>=0.0.23",
     ],
     extras_require={
         "code_style": ["flake8<3.8.0,>=3.7.0", "black", "pre-commit==1.17.0"],
