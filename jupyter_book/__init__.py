256036091c5e2eb320393d973bbac3d503aef37c
Chris Holdgraf <choldgraf@berkeley.edu>
2020-07-29 12:46:04 -0700

    🚀 RELEASE: v0.7.3

--- jupyter_book/__init__.py
+++ jupyter_book/__init__.py
@@ -5,7 +5,7 @@ from .toc import update_indexname, add_toctree
 from .directive.toc import TableofContents, SwapTableOfContents
 
 
-__version__ = "0.7.2"
+__version__ = "0.7.3"
 
 
 def add_static_files(app, config):
