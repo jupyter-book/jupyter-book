662cefdbbc73474d3688730ae0a08aa76323fc3b
Andrew Sanchez <inbox.asanchez@gmail.com>
2020-08-06 14:00:29 -0700

    Stop mutating test data within the cwd

--- tests/test_toc.py
+++ tests/test_toc.py
@@ -8,14 +8,14 @@ from bs4 import BeautifulSoup
 path_books = Path(__file__).parent.joinpath("books")
 
 
-def test_toc():
-    path_book = Path(__file__).parent.joinpath("books", "toc")
-    run(f"jb toc {path_book}".split(), check=True)
-    toc_yaml = path_book.joinpath("_toc.yml")
+def test_toc(build_resources):
+    books, tocs = build_resources
+    run(f"jb toc {tocs}".split(), check=True)
+    toc_yaml = tocs.joinpath("_toc.yml")
     _ = yaml.safe_load(toc_yaml.read_text(encoding="utf8"))
 
     # Folder with no content should return none
-    p_empty = path_book.parent
+    p_empty = tocs.parent
     with pytest.raises(ValueError):
         out = run(f"jb toc {p_empty}".split(), stderr=PIPE, stdout=PIPE)
         err = out.stderr.decode()
@@ -24,17 +24,17 @@ def test_toc():
     assert "No content files were found in" in err
 
 
-def test_toc_add_titles():
-    path_book = path_books.joinpath("toc")
-    run(f"jb toc {path_book}".split(), check=True)
-    toc_yaml = path_book.joinpath("_toc.yml")
+def test_toc_add_titles(build_resources):
+    books, tocs = build_resources
+    run(f"jb toc {tocs}".split(), check=True)
+    toc_yaml = tocs.joinpath("_toc.yml")
     res = yaml.safe_load(toc_yaml.read_text(encoding="utf8"))
     assert "title" not in res
     for section in res["sections"]:
         assert "title" not in section
 
-    run(f"jb toc {path_book} --add-titles".split(), check=True)
-    toc_yaml = path_book.joinpath("_toc.yml")
+    run(f"jb toc {tocs} --add-titles".split(), check=True)
+    toc_yaml = tocs.joinpath("_toc.yml")
     res = yaml.safe_load(toc_yaml.read_text(encoding="utf8"))
     assert "title" in res
     for section in res["sections"]:
