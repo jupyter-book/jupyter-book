00f39dacac3c5d72d31c8524f992dac2416e3dae
Chris Holdgraf <choldgraf@berkeley.edu>
2020-11-07 18:06:35 -0600

    reverting version

--- setup.py
+++ setup.py
@@ -66,7 +66,7 @@ setup(
     install_requires=[
         "pyyaml",
         "docutils>=0.15",
-        "sphinx>=2,<3.3",
+        "sphinx>=2,<4",
         "myst-nb~=0.10.1",
         "click",
         "setuptools",
