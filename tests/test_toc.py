a1701f2b4311ee33a84345ea06e84d7a0af169af
Chris Sewell <chrisj_sewell@hotmail.com>
2021-04-18 17:34:30 +0200

    uncomment test_toc.py

--- tests/test_toc.py
+++ tests/test_toc.py
@@ -1,48 +1,50 @@
-# from pathlib import Path
+from pathlib import Path
 
-# from click.testing import CliRunner
-# import yaml
-# from bs4 import BeautifulSoup
+import yaml
+from click.testing import CliRunner
+from sphinx_external_toc.cli import create_toc
 
-# from jupyter_book.cli.main import build, toc
-
-# path_books = Path(__file__).parent.joinpath("books")
+path_books = Path(__file__).parent.joinpath("books")
 
 
-# def test_toc_basic(cli: CliRunner, build_resources):
-#     books, tocs = build_resources
-#     # run(f"jb toc {tocs}".split(), check=True)
-#     result = cli.invoke(toc, tocs.as_posix())
-#     assert result.exit_code == 0
-
-#     toc_yaml = tocs.joinpath("_toc.yml")
-#     _ = yaml.safe_load(toc_yaml.read_text(encoding="utf8"))
+def test_toc_basic(cli: CliRunner, build_resources):
+    books, tocs = build_resources
+    # run(f"jb toc from-site {tocs} -f jb-article".split(), check=True)
+    result = cli.invoke(create_toc, [tocs.as_posix()])
+    assert result.exit_code == 0
+    yaml.safe_load(result.output)
 
 
-# def test_toc_fail(cli: CliRunner, build_resources):
-#     """Folder with no content should return none"""
-#     books, tocs = build_resources
-#     p_empty = tocs.parent
-#     result = cli.invoke(toc, p_empty.as_posix())
-#     assert result.exit_code != 0
-#     assert isinstance(result.exception, RuntimeError)
-#     assert "No content files were found in" in str(result.exception)
+def test_toc_fail(cli: CliRunner, build_resources):
+    """Folder with no content should return none"""
+    books, tocs = build_resources
+    p_empty = tocs.parent
+    result = cli.invoke(create_toc, [p_empty.as_posix()])
+    assert result.exit_code != 0
+    assert isinstance(result.exception, OSError)
+    assert "path does not contain a root file" in str(result.exception)
 
 
-# def test_toc_add_titles(cli: CliRunner, build_resources):
-#     books, tocs = build_resources
-#     result = cli.invoke(toc, tocs.as_posix())
-#     assert result.exit_code == 0
-#     toc_yaml = tocs.joinpath("_toc.yml")
-#     res = yaml.safe_load(toc_yaml.read_text(encoding="utf8"))
-#     assert "title" not in res
-#     for section in res["sections"]:
-#         assert "title" not in section
+def test_toc_add_titles(cli: CliRunner, build_resources):
+    books, tocs = build_resources
+    result = cli.invoke(create_toc, [tocs.as_posix(), "-f", "jb-article"])
+    assert result.exit_code == 0
+    res = yaml.safe_load(result.output)
+    assert "title" not in res
+    assert "sections" in res
+    for section in res["sections"]:
+        assert "title" not in section
 
-#     result = cli.invoke(toc, (tocs.as_posix(), "--add-titles"))
-#     assert result.exit_code == 0
-#     toc_yaml = tocs.joinpath("_toc.yml")
-#     res = yaml.safe_load(toc_yaml.read_text(encoding="utf8"))
-#     assert "title" in res
-#     for section in res["sections"]:
-#         assert "title" in section
+    result = cli.invoke(
+        create_toc, [tocs.as_posix(), "-f", "jb-article", "--guess-titles"]
+    )
+    assert result.exit_code == 0
+    res = yaml.safe_load(result.output)
+    # print(result.output)
+    # assert "title" in res
+    assert "sections" in res
+    for section in res["sections"]:
+        assert "title" in section
+
+
+# note: build tests moved to test_build
