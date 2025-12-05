563df774f6fa08280f25ca5552b2c65021f20a84
Freek Pols <55780866+FreekPols@users.noreply.github.com>
2025-12-05 19:30:42 +0100

    randomized gallery (#2512)
    should close https://github.com/jupyter-book/jupyter-book/issues/2508
    
    created shuffled item list
    move myst docs to gallery to be on top
    removed myst docs from gallery.yml to prevent duplication

--- docs/src/create_gallery.py
+++ docs/src/create_gallery.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 import yaml
 import html
+import random
 
 SRC = Path("gallery.yml")
 DST = Path("_build/temp/gallery.txt")
@@ -10,6 +11,7 @@ def esc(t: str) -> str:
     return html.escape(t or "", quote=False)
 
 items = yaml.safe_load(SRC.read_text(encoding="utf-8"))
+random.shuffle(items)
 
 lines = []
 
